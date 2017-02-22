a0d2f464aadeb6b773ef4cb6046640c7d81c6403
mathew <adysys@yahoo.com>
2017-02-22 15:46:34 +0100

    my django girls app, first commit

---
+++ myblog/blog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from myblog.blog.models import Post
+
+admin.site.register(Post)
---
+++ myblog/blog/models.py
@@ -0,0 +1,18 @@
+import datetime
+from django.db import models
+from django.utils import tzinfo
+
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=datetime.datetime.today())
+	published_date = models.DateTimeField(blank=True, null=True)
+
+
+	def publish(self):
+		self.published_date = datetime.datetime.today()
+		self.save()
+
+	def __str__(self):
+		return self.title
